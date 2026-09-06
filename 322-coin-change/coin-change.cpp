1041f76c0dfcfcb530d5eacbdfd6f105a5e3df64
AyushKumarexe <24bma009@nith.ac.in>
2026-09-06 18:49:40 +0530

    Time: 47 ms (35.70%) | Memory: 48.9 MB (38.75%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -21,8 +21,29 @@ class Solution {
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
-       vector<vector<int>>dp(coins.size(),vector<int>(amount +1,INT_MAX));
-       int ans =  dfs(coins.size()-1,amount,coins,dp);
+   int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 1e8));
+        
+        // Base case: i == 0, only first coin available
+        for (int target = 0; target <= amount; target++) {
+            if (target % coins[0] == 0)
+                dp[0][target] = target / coins[0];
+            // else stays 1e8 (impossible)
+        }
+        
+        // Fill rest of the table
+        for (int i = 1; i < n; i++) {
+            for (int target = 0; target <= amount; target++) {
+                int skip = dp[i - 1][target];
+                int take = 1e8;
+                if (coins[i] <= target) {
+                    take = 1 + dp[i][target - coins[i]];
+                }
+                dp[i][target] = min(skip, take);
+            }
+        }
+        
+        int ans = dp[n - 1][amount];
         return ans >= 1e8 ? -1 : ans;
     }
 };
